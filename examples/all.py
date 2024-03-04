d6f3d507cd0ea265de5862c76aec47f77ff3bf62
Javier Luraschi <jluraschi@gmail.com>
2024-03-04 15:02:52 -0800

    fix to all controls example

--- examples/all.py
+++ examples/all.py
@@ -123,7 +123,7 @@ data = [
     { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016/12/15 06:00:53', 'gender': 'Male' },
     { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016/04/26 06:26:28', 'gender': 'Female' },
     { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016/04/10 10:28:46', 'gender': 'Male' },
-    { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016/12/06 14:38:38', 'gender': 'Female' }
+    { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016/12/06 14:38:38', 'gender': 'Female' },
     { 'id': 6, 'first_name': 'Johnny', 'last_name': 'Smith', 'date': '2015/11/06 11:12:43', 'gender': 'Male' }
 ]
 sy.table(data=data)
