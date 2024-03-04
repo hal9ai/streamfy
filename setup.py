0a937521c77803d966934813a833c857a0739263
Javier Luraschi <jluraschi@gmail.com>
2024-03-04 15:01:46 -0800

    improve readme with missing fields

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamfy",
-    version="0.2.8",
-    author="",
-    author_email="",
-    description="",
-    long_description="",
-    long_description_content_type="text/plain",
-    url="",
+    version="0.2.9",
+    author="Javier Luraschi",
+    author_email="jluraschi@gmail.com",
+    description="Modern frontend web components based on Buefy for Streamlit.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/hal9ai/streamfy",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.6",
     install_requires=[
-        # By definition, a Custom Component depends on Streamlit.
-        # If your component has other Python dependencies, list
-        # them here.
         "streamlit >= 0.63",
     ],
 )
