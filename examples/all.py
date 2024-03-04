dd89188c60a903594295ed2eb9a7939138280a2c
Javier Luraschi <jluraschi@gmail.com>
2024-03-04 14:56:09 -0800

    use pagination in table example

--- examples/all.py
+++ examples/all.py
@@ -118,20 +118,12 @@ sy.steps(steps=[
 ])
 
 st.subheader("Table")
-columns = [
-    {
-        "field": 'id',
-        "label": 'ID',
-        "width": '40',
-        "numeric": True
-    },
-    {
-        "field": 'name',
-        "label": 'Name',
-    },
-]
 data = [
-    { 'id': 1, 'name': 'Jesse' },
-    { 'id': 2, 'first_name': 'John' },
+    { 'id': 1, 'first_name': 'Jesse', 'last_name': 'Simmons', 'date': '2016/10/15 13:43:27', 'gender': 'Male' },
+    { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016/12/15 06:00:53', 'gender': 'Male' },
+    { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016/04/26 06:26:28', 'gender': 'Female' },
+    { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016/04/10 10:28:46', 'gender': 'Male' },
+    { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016/12/06 14:38:38', 'gender': 'Female' }
+    { 'id': 6, 'first_name': 'Johnny', 'last_name': 'Smith', 'date': '2015/11/06 11:12:43', 'gender': 'Male' }
 ]
-sy.table(data=data, columns=columns, paginated=True)
+sy.table(data=data)
