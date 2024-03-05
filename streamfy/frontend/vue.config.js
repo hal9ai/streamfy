bfe6170fe267d51505585d08264846cf6a2d6431
Javier Luraschi <jluraschi@gmail.com>
2024-03-05 14:20:01 -0800

    add support to customize webpack config

--- streamfy/frontend/vue.config.js
+++ streamfy/frontend/vue.config.js
@@ -1,3 +1,11 @@
+// npx vue-cli-service inspect > webpack.js
+
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  chainWebpack: config => {
+    config.optimization.minimizer('terser').tap(args => {
+      // args[0].terserOptions.compress.dead_code = false;
+      return args;
+    });
+  }
 }
